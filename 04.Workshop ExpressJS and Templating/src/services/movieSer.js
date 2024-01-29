d786cb5f8c9d957960f64548758a7c1f66cea391
ivailospasov85 <133914017+ivailospasov85@users.noreply.github.com>
2024-01-30 00:08:54 +0200

    add hardcoded movie

--- 04.Workshop ExpressJS and Templating/src/services/movieSer.js
+++ 04.Workshop ExpressJS and Templating/src/services/movieSer.js
@@ -1,4 +1,12 @@
-movies = []
+movies = [{
+    title: 'Jungle Cuise 222',
+    genre: 'Action',
+    director: 'iv4o',
+    data: '2024',
+    imageUrl: 'ads',
+    rating: '10',
+    description: 'na iv4o filma'
+}]
 exports.create = (movieData) => {
     movies.push(movieData)
 }
