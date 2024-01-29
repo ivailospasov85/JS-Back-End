181748c0cb9f45c0ea21d94d0e386035887a6e0f
ivailospasov85 <133914017+ivailospasov85@users.noreply.github.com>
2024-01-29 19:51:08 +0200

    add about page

--- 04.Workshop ExpressJS and Templating/src/controller/homeController.js
+++ 04.Workshop ExpressJS and Templating/src/controller/homeController.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 
 
 router.get('/', (req, res) => {
-    res.render('home', { layout: false })
+    res.render('home')
 })
 
-// router.get('/about',)
+router.get('/about',(req,res)=>{
+    res.render('about')
+})
 
 module.exports = router
