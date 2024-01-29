e8227d22baaeed3ad8cce8905f4cd7b6222ee1b2
ivailospasov85 <133914017+ivailospasov85@users.noreply.github.com>
2024-01-29 20:57:00 +0200

    add 404 page not found

--- 04.Workshop ExpressJS and Templating/src/routes.js
+++ 04.Workshop ExpressJS and Templating/src/routes.js
@@ -2,11 +2,15 @@ const router = require('express').Router()
 
 const homeController = require('./controller/homeController')
 const movieController=require('./controller/movieController')
-
+const error404Controller=require('./controller/error404Controller')
 
 
 
 router.use(homeController)
 router.use(movieController)
+
+
+
+router.use(error404Controller)
 
 module.exports = router
