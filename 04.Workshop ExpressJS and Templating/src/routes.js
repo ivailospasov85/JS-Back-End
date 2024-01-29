16793ef36e0537422ddb0fc5eb8f46b1859e89a2
ivailospasov85 <133914017+ivailospasov85@users.noreply.github.com>
2024-01-29 20:01:08 +0200

    add create movie page

--- 04.Workshop ExpressJS and Templating/src/routes.js
+++ 04.Workshop ExpressJS and Templating/src/routes.js
@@ -1,7 +1,12 @@
 const router = require('express').Router()
 
 const homeController = require('./controller/homeController')
+const movieController=require('./controller/movieController')
+
+
+
 
 router.use(homeController)
+router.use(movieController)
 
 module.exports = router
