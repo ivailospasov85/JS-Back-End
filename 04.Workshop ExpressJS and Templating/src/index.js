e796864a8b451afb63a077e1c7339cfbab7e908a
ivailospasov85 <133914017+ivailospasov85@users.noreply.github.com>
2024-01-29 17:06:24 +0200

    add express-handlebars

--- 04.Workshop ExpressJS and Templating/src/index.js
+++ 04.Workshop ExpressJS and Templating/src/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
+const path = require('path')
+
 
 const app = express()
 const port = 5000
 
-app.get('/',(req,res)=>{
-    res.send('Home page')
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}))
+
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+    res.render('home', { layout: false })
 })
 
-app.listen(port,()=>console.log(`Server is listening ot port ${port}`))
+app.listen(port, () => console.log(`Server is listening ot port ${port}`))
