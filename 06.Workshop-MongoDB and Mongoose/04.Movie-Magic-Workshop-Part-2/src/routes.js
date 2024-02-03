1a0f14adc55a15afbe5ed65e2a978212a4e7ac60
ivailospasov85 <133914017+ivailospasov85@users.noreply.github.com>
2024-02-03 11:53:19 +0200

    added Cast

--- 06.Workshop-MongoDB and Mongoose/04.Movie-Magic-Workshop-Part-2/src/routes.js
+++ 06.Workshop-MongoDB and Mongoose/04.Movie-Magic-Workshop-Part-2/src/routes.js
@@ -1,13 +1,15 @@
 const router = require('express').Router()
 
 const homeController = require('./controller/homeController')
-const movieController=require('./controller/movieController')
-const error404Controller=require('./controller/error404Controller')
+const movieController = require('./controller/movieController')
+const error404Controller = require('./controller/error404Controller')
+const castController = require('./controller/castController')
 
 
 
 router.use(homeController)
 router.use(movieController)
+router.use(castController)
 
 
 
