78a0267076309199b8c163347cd5abd7b7bbf63c
ivailospasov85 <133914017+ivailospasov85@users.noreply.github.com>
2024-02-07 23:12:32 +0200

    hash password on save

--- 08.Workshop Session and Authentication/Magic-Workshop-Part-3/src/models/User.js
+++ 08.Workshop Session and Authentication/Magic-Workshop-Part-3/src/models/User.js
@@ -1,15 +1,23 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt')
 
 const userScheme = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        lowerCase:true
+        lowerCase: true
     },
     password: {
         type: String,
-        required: true
+        required: true,
+
     }
+})
+
+userScheme.pre('save', async function () {
+
+    const hash = await bcrypt.hash(this.password, 12)
+    this.password = hash
 })
 
 const User = mongoose.model('User', userScheme)
