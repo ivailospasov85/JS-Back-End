94012c21d1f46ab1c2b88298a3d4796ad506a182
ivailospasov85 <133914017+ivailospasov85@users.noreply.github.com>
2024-02-09 22:35:28 +0200

    added edit page

--- 08.Workshop Session and Authentication/Magic-Workshop-Part-3/src/controller/authController.js
+++ 08.Workshop Session and Authentication/Magic-Workshop-Part-3/src/controller/authController.js
@@ -28,7 +28,8 @@ router.post('/login', async (req, res) => {
 
     const token = await authService.login(email, password)
 
-    console.log(token);
+    res.cookie('auth',token)
+
 
     res.redirect('/')
 
